f8c205d957a33c0c09753d9f929f7060e8020069
TaHuC <TaHuC@hotmail.com>
2017-01-23 22:51:20 +0200

    My first home work

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CenturiesToMinutes
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			Console.Write("Centuries = ");
+			int centuries = int.Parse(Console.ReadLine());
+			int years = centuries * 100;
+			int days = (int)(years * 365.2422);
+			int hours = 24 * days;
+			int minutes = 60 * hours;
+
+			Console.WriteLine($"{centuries} centuries = {years} years = {days} days = {hours} hours = {minutes} minutes");
+
+		}
+	}
+}
